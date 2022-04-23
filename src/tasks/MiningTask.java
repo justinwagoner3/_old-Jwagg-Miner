543a9b1db84b3524c63fbd680d703894864fac4e
Justin Wagoner <justin.wagoner@koriist.com>
2022-04-23 16:26:10 -0700

    update `isMining` to include if the player is walking; lower the max sleep time after clicking "Mine" to increase chance of "double-click"

--- src/tasks/MiningTask.java
+++ src/tasks/MiningTask.java
@@ -25,29 +25,18 @@ public class MiningTask extends TaskNode {
         if (rock == null) return Calculations.random(500, 1000);
 
         if (rock.interact("Mine")) { // If we successfully click on the rock
-            sleepUntil(this::isMining, Calculations.random(1000, 2000)); // Wait until we're mining, with a max wait time
+            sleepUntil(this::isMining, Calculations.random(100, 2500)); // Wait until we're mining, with a max wait time
         }
 
         return Calculations.random(200, 500);
     }
 
-    /**
-     * Finds the closest acceptable rock that we can mine
-     *
-     * @return The closest GameObject that has the name 'Rocks', with the action 'Mine', and non-null model colors
-     */
     private GameObject getClosestRock() {
         return GameObjects.closest(object -> object.getName().equalsIgnoreCase("Rocks") && object.hasAction("Mine") && object.getModelColors() != null && object.getModelColors()[0] == ironOre);
     }
 
-    /**
-     * For part 1, we'll consider our player doing any animation as mining
-     * This will be improved/changed in a future part
-     *
-     * @return true if the player is mining, otherwise false
-     */
     private boolean isMining() {
-        return getLocalPlayer().isAnimating();
+        return getLocalPlayer().isAnimating() || getLocalPlayer().isMoving();
     }
 
 }
